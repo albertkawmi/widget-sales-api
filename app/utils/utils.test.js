6b8ada479d04d7d76299cf41067d4f07cece72b0
Albert Kawmi <albert.kawmi@gmail.com>
2018-01-18 00:09:55 +0000

    Add unit tests for loggingService

---
+++ app/utils/utils.test.js
@@ -0,0 +1,27 @@
+/* eslint-env jest */
+const { loggingService } = require('./')
+
+const consoleLog = jest.spyOn(console, 'log')
+  .mockImplementation(() => {})
+
+// Mocks
+const req = {
+  method: 'GET',
+  originalUrl: '/clients',
+  hostname: 'localhost',
+  ip: '1.2.3.4'
+}
+const res = {}
+const next = () => {}
+
+test('loggingService', () => {
+  process.env.NODE_ENV = 'test'
+  loggingService(req, res, next)
+  expect(consoleLog).not.toHaveBeenCalled()
+
+  process.env.NODE_ENV = 'production'
+  loggingService(req, res, next)
+  expect(consoleLog).toHaveBeenCalledWith(
+    expect.stringMatching(/GET \/clients localhost 1\.2\.3\.4/)
+  )
+})
